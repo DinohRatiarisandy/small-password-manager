0cb479ac388a66648dcda494f75accadd8047013
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2026-06-24 06:13:07 +0300

    feat: make the relationship in organisation

--- app/models/organisation.ts
+++ app/models/organisation.ts
@@ -1,4 +1,13 @@
 import { OrganisationSchema } from '#database/schema'
+import { belongsTo, hasMany } from '@adonisjs/lucid/orm'
+import User from './user.ts'
+import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
+import OrganisationMember from './organisation_member.ts'
 
 export default class Organisation extends OrganisationSchema {
+  @belongsTo(() => User)
+  declare owner: BelongsTo<typeof User>
+
+  @hasMany(() => OrganisationMember)
+  declare members: HasMany<typeof OrganisationMember>
 }
