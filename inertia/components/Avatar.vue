00cdd7b628a366cc72617a43b913ee9584120688
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2026-06-28 21:46:23 +0300

    feat: avatar component

---
+++ inertia/components/Avatar.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { cn } from 'tailwind-variants'
+
+const props = defineProps<{
+  initials?: string
+  dataTip?: string
+  tooltipPosition?: 'top' | 'bottom' | 'left' | 'right'
+}>()
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'tooltip avatar avatar-placeholder',
+        props.tooltipPosition && `md:tooltip-${props.tooltipPosition} md:tooltip-center`
+      )
+    "
+    :data-tip="props.dataTip"
+  >
+    <div class="bg-neutral text-neutral-content w-8 rounded-full">
+      <span class="text-xs font-bold">
+        {{ initials }}
+      </span>
+    </div>
+  </div>
+</template>
