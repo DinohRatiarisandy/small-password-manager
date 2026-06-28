7e8e4f7c4e2cddadfdc07367935a8f2ae27afd7c
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2026-06-28 18:19:30 +0300

    feat: list all user's organisations

---
+++ inertia/pages/organisation/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Organisation from '#models/organisation'
+
+defineProps<{
+  organisations: Organisation[]
+}>()
+</script>
+
+<template>
+  <h1 class="text-2xl">Available organisations</h1>
+
+  <ul>
+    <li v-for="organisation in organisations" :key="organisation.id">
+      {{ organisation.name }}
+    </li>
+  </ul>
+</template>
