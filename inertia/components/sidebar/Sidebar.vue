b96ff44191d4eef2ba52145770b428f5c3078ee1
dinohsandys <dinoh.ratiarisandy@peoplepforai.com>
2026-06-30 16:51:04 +0300

    feat: sidebar component

---
+++ inertia/components/sidebar/Sidebar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import CategoriesList from './CategoriesList.vue'
+import OrganisationsList from './OrganisationsList.vue'
+</script>
+
+<template>
+  <nav>
+    <CategoriesList />
+    <OrganisationsList />
+  </nav>
+</template>
