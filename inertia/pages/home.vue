5d71e4e828a186f803766174d5fb54e935c8d83a
dinohsandys <dinoh.ratiarisandy@peoplepforai.com>
2026-06-30 16:51:33 +0300

    feat: home page

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import { Link } from '@adonisjs/inertia/vue'
 import { Head } from '@inertiajs/vue3'
 </script>
 
 <template>
   <Head title="Homepage" />
-
   <div>
+    <Link route="organisations.index"><a class="link link-primary">Organisations</a></Link>
     <h1 class="text-3xl">HOME PAGE</h1>
   </div>
 </template>
